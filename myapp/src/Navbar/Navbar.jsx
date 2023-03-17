d190ae1473e4a2e86604ad18cdf6f8b05103f1b5
Ashutosh5333 <lakshakarashutosh@gmail.com>
2023-03-17 12:07:19 +0530

    navbar done

---
+++ myapp/src/Navbar/Navbar.jsx
@@ -0,0 +1,41 @@
+import React from 'react'
+import "./Nav.css"
+import {AiOutlineHome} from "react-icons/ai"
+import {BsTools} from "react-icons/bs"
+import {IoIosNotificationsOutline} from "react-icons/io"
+import {CgProfile} from "react-icons/cg"
+
+const Navbar = () => {
+
+  return (
+    <>
+     <nav className='Continer-nav'>
+
+        <div className='nav-icon'>
+            <img src="https://deepthought.education/assets/images/logo/logo.svg" alt="image"/>
+        </div>
+
+         <div className='Nav-icons'> 
+         <div className='nav-home'> 
+            <AiOutlineHome className='home'/>
+         </div>
+         <div
+         className='nav-home'> 
+          <BsTools className='home'/>
+          </div>
+
+         <div className='nav-home'> 
+           <IoIosNotificationsOutline  className='home'/>
+         </div>
+         <div className='nav-home'> 
+          <CgProfile className='home'/>
+          </div>
+
+         </div>
+
+     </nav>
+    </>
+  )
+}
+
+export default Navbar
